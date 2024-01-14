e69b927cc7371bb36a8e7c1879c3a2fb18e72c11
BranislavDokic <143610751+BranislavDokic@users.noreply.github.com>
2024-01-14 23:57:40 +0100

    commit4 log in form and createAcc

---
+++ GymNte/CreateAccPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GymNote
+{
+    /// <summary>
+    /// Interaction logic for CreateAccPage.xaml
+    /// </summary>
+    public partial class CreateAccPage : Page
+    {
+        public CreateAccPage()
+        {
+            InitializeComponent();
+        }
+
+        private void CreateButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ GymNte/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using GymNote;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GymNte
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void CreateAccButton_Click(object sender, RoutedEventArgs e)
+        {
+            //CreateAccPage createAccPage = new CreateAccPage();
+            //NavigationService navService = NavigationService.GetNavigationService(this);
+            //navService.Navigate(createAccPage);
+            CreateAccPage createAccPage = new CreateAccPage();
+            this.Content = createAccPage;
+        }
+    }
+}
